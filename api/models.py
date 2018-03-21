fb790305f37b668b2c1ab03c82cad63005ffc3ca
Kentaro KAZAMA <kazehara@outlook.com>
2018-03-21 18:43:57 +0900

    Add Article model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+
+    ALL = 0
+
+    # 記事タイトル
+    title = models.CharField(max_length=256)
+    # 記事カテゴリ
+    category = models.CharField(max_length=64)
+    # 担当者
+    administrator = models.CharField(max_length=64)
+    # 本文
+    body = models.CharField(max_length=4096)
+    # 発信元
+    publisher = models.CharField(max_length=64)
+
+    # 添付ファイル名
+    attach_name = models.CharField(max_length=128)
+    # 添付ファイルURL
+    attach_url = models.URLField()
+
+    # 対象学科年
+    target = models.IntegerField()
